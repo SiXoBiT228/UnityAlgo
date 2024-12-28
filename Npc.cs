1c778a9b15856a4590ebb75b01431c48054eae6e
SiXoBiT228 <superkrasiska@gmail.com>
2024-12-28 17:05:08 +0800

    Add files via upload

---
+++ Npc.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Npc : MonoBehaviour
+{
+    //Здоровье NPC
+    public int health = 5;
+
+    //Уровень NPC
+    public int level = 1;
+
+    //Скорось NPC
+    public float speed = 1.2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
+
